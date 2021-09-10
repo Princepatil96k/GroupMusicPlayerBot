e9f883f209840eebe555eeceb0d1122f60fe1bd3
harsh2429 <85775284+harsh2429@users.noreply.github.com>
2021-09-10 11:21:21 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -11,7 +11,7 @@ async def start(_, message: Message):
     await message.reply_text(
         f"""**Hey, I'm {bn} 🎵
 
-I can play music in your group's voice call. And Developed By [𝗛𝗔𝗥𝗦𝗛](https://t.me/Harsh_722).
+I can play music in your group's voice call. And Developed By - [𝗛𝗔𝗥𝗦𝗛](https://t.me/Harsh_722).
 
 Add me to your group and play music freely!**
         """,
