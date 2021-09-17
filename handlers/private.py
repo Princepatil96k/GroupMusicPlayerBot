8b66aaece45e2bc937bf5bf85d88bde508f0605c
PrincePatil <90929817+Princepatil96k@users.noreply.github.com>
2021-09-18 00:39:04 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,26 +7,26 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_photo("https://telegra.ph/file/ed4f68f51b48861989d6e.jpg")
+    await message.reply_photo("https://telegra.ph/file/9c71bd26b7772df92684a.jpg")
     await message.reply_text(
-        f"""**Hey, I'm 『𝙿𝚁𝙸𝙽_𝚂𝙴𝙲𝙲 𝙼𝚄𝚂𝙸𝙲』 Bᴏᴛ |🎵
+        f"""**Hey, I'm 『PRINCE PATIL MUSIC』 Bᴏᴛ |🎵
 
-I can play ꬺᶙȿᶖɕ  in your group's voice CHAT Developed by [『𝙿𝚁𝙸𝙽_𝚂𝙴𝙲𝙲✰』](https://t.me/PRIN_SECC)
+I can play ꬺᶙȿᶖɕ  in your group's voice CHAT Developed by [『🔰⚡𝙿𝚁𝙸NICE PATIL⚡🔰✰』](https://t.me/Princepatil96k)
 
-Add me to your group and play music freely😆!**
+Add me to your group and play music freely @STUDY_FLOWER_QUIZ_GROUOP 😆!**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "𝐎𝐖𝐍𝐄𝐑", url="https://t.me/PRIN_SECC")
+                        "𝐎𝐖𝐍𝐄𝐑", url="https://t.me/Princepatil96k")
                   ],[
                     InlineKeyboardButton(
-                        "𝐒𝐔𝐏𝐏𝐎𝐑𝐓", url="https://t.me/Ak_Dukhi_Aatma"
+                        "𝐒𝐔𝐏𝐏𝐎𝐑𝐓", url="https://t.me/PrinceMusicWorld1"
                     ),
                 ],[ 
                     InlineKeyboardButton(
-                        "GROUP ME LEJAO 😆", url="https://t.me/PRINSECC_VC_BOT?startgroup=true"
+                        "PRINCE PATIL MUSIC KO LE JAO😆", url="https://t.me/PRINCE_PATIL_OP_BOT?startgroup=true"
                     )]
             ]
         ),
@@ -40,7 +40,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "𝐒𝐔𝐏𝐏𝐎𝐑𝐓", url="https://t.me/Ak_Dukhi_Aatma")
+                        "𝐒𝐔𝐏𝐏𝐎𝐑𝐓", url="https://t.me/Princepatil96k")
                 ]
             ]
         )
